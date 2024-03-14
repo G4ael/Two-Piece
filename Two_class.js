5d26c03a2bb5c7d555d60e4ddf5bf4ed081bcd28
JOÃO VICTOR CARVALHO NICOLAI <joao_nicolai@estudante.sesisenai.org.br>
2024-03-14 11:26:39 -0300

    tiro, andar, colisão e inimigos

--- Two_class.js
+++ Two_class.js
@@ -10,6 +10,24 @@ class Obj{
         des.fillStyle = this.a
         des.fillRect(this.x,this.y,this.w,this.h,this.a)
     }
+
+    des_img(){
+        let img = new Image()
+        img.src = this.a
+        des.drawImage(img,this.x, this.y, this.w, this.h)
+    }
+
+    colid(objeto) {
+        if((this.x < objeto.x + objeto.w)&&
+            (this.x + this.w > objeto.x)&&
+            (this.y < objeto.y + objeto.h)&&
+            (this.y + this.h > objeto.y)){
+            return true     
+        }else{
+            return false
+        }
+    }
+
 }
 
 class Player extends Obj{
@@ -29,23 +47,40 @@ class Player extends Obj{
 }
 
 class Inimigo extends Obj{
+    vida_inimigo = 5
     pararY = 180  // posição onde o inimigo vai parar
 
     atual_inimigo(){
         if(this.y <= this.pararY){
-            this.y += 0.5
+            this.y += 1.5
+        }else if(this.vida_inimigo <= 0){
+            this.recomeca()
         }
+    }
+    recomeca(){
+        this.y = -100
+        this.x = Math.floor(Math.random() * ((950 - 2 + 1) + 2)) // quando o inimigo morrer
+    }
+
+    vel = Math.random() * (1 - 0.5) + 0.5
+
+    mov(){
+        if(this.y <= this.pararY){
+            this.y += 1.5
+        }
+
     }
 }
 
 class Tiro extends Obj{
 des_tiro(){
-        des.fillStyle = this.a
-        des.fillRect(this.x, this.y, this.w, this.h)
+        let img = new Image()
+        img.src = this.a
+        des.drawImage(img,this.x, this.y, this.w, this.h)
     }
 
     mov(){
-        this.y -= 10
+        this.y -= 5
     }
 }
 
--- Two_main.js
+++ Two_main.js
@@ -1,7 +1,9 @@
 let des =document.getElementById('des').getContext('2d')
 
 let player = new Player(100,500,50,50,'red') //mudar o x,y,h,h,w,a
-let inimigo = new Inimigo(500,-100,50,50,'yellow') //mudar o x,y,h,h,w,a
+//let inimigo = new Inimigo(500,-100,50,50,'yellow') //mudar o x,y,h,h,w,a
+//let inimigo2 = new Inimigo(300,-80,50,50,'yellow') //mudar o x,y,h,h,w,a
+//let inimigo3 = new Inimigo(700,-150,50,50,'yellow') //mudar o x,y,h,h,w,a
 let text1 = new Text(100,100,50,50,'red') //mudar o x,y,h,h,w,a
 let text2 = new Text(100,100,50,50,'red') //mudar o x,y,h,h,w,a
 let text3 = new Text(100,100,50,50,'red') //mudar o x,y,h,h,w,a
@@ -32,6 +34,68 @@ let tiros = {
     }
 }
 
+let grupoInimigo = []
+let inimigo = {
+    time1: 0, 
+    time2: 0,
+    time3: 0,
+
+    criaInimigo(){
+        this.time1 += 0.2
+        this.time2 += 0.2
+        this.time3 += 0.2
+        let pos_x = (Math.random() * (900 - 2 +1)+2)
+        let pos_x2 = (Math.random() * (900 - 2 +1)+2)
+        let pos_x3 = (Math.random() * (900 - 2 +1)+2)
+        if(this.time1 >=60){
+            this.time1 = 0
+            grupoInimigo.push(new Inimigo(pos_x,-200,50,50,'yellow'))
+            console.log(grupoInimigo)
+        }
+        if(this.time2 >=85){
+            this.time2 = 0
+            grupoInimigo.push(new Inimigo(pos_x2,-300,50,50,'yellow'))
+            console.log(grupoInimigo)
+        }
+        if(this.time3 >=135){
+            this.time3 = 0
+            grupoInimigo.push(new Inimigo(pos_x3,-400,50,50,'yellow'))
+            console.log(grupoInimigo)
+        }
+    },
+    des(){
+        grupoInimigo.forEach((inimigo)=>{
+            inimigo.des_obj()
+        })
+    },
+    destroiInimigo(){
+        grupoTiros.forEach((tiro)=>{
+            grupoInimigo.forEach((inimigo)=>{
+                if(tiro.colid(inimigo)){
+                    grupoTiros.splice(grupoTiros.indexOf(tiro), 1)
+                    grupoInimigo.splice(grupoInimigo.indexOf(inimigo), 1)
+                    player.pts += 1
+                }
+            })
+        })
+    },
+    atual(){
+        this.criaInimigo()
+        this.destroiInimigo()
+        grupoInimigo.forEach((inimigo)=>{
+            inimigo.mov()
+        })
+    }
+}
+
+//tiro
+document.addEventListener('keypress', (ev)=>{
+    if (ev.key === 'l') { //tecla para atirar
+        grupoTiros.push(new Tiro(player.x - 15 + player.w / 4, player.y, 30, 40, './assets/tiro.png')) 
+        // da onde o tiro vai sair
+    }
+})
+
 document.addEventListener('keydown',(e)=>{
     // console.log(e.key)
     if(e.key === 'a'){
@@ -48,12 +112,6 @@ document.addEventListener('keyup', (e)=>{
     }
 })
 
-//tiro
-document.addEventListener('keypress', (ev)=>{
-    if (ev.key === 'l') {
-        grupoTiros.push(new Tiro(player.x - 4 + player.w / 2, player.y, 8, 16, 'assets/tiro.png'))
-    }
-})
 
 
 function desenha(){
@@ -63,13 +121,17 @@ function desenha(){
     text4.des_text(player.vida,100, 21, 'White', '26px Times')
 
     player.des_obj()
-    inimigo.des_obj()
+    inimigo.des()
+    //inimigo2.des_obj()
+    //inimigo3.des_obj()
     tiros.des()
 }
 
 function atualiza(){
     player.mov()
-    inimigo.atual_inimigo()
+    inimigo.atual()
+    //inimigo2.atual_inimigo()
+    //inimigo3.atual_inimigo()
     tiros.atual()
 }
 
