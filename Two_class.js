ec9eef7e02b586161ac1cd5cc66587428ea3e685
Luiz <luiz_f_maria@estudante.sesisenai.org.br>
2024-03-14 11:19:59 -0300

    Layout da pagina

--- Two_class.js
+++ Two_class.js
@@ -11,27 +11,52 @@ class Obj{
         img.src = this.a
         des.drawImage(img, this.x, this.y, this.w, this.h)
     }
+    des_obj(){
+        des.fillStyle = this.a
+        des.fillRect(this.x,this.y,this.w,this.h,this.a)
+    }
 
 }
 
-class Armas extends Obj(){
-    
+class Armas extends Obj{
+    dir = 0
+    mov(){
+        this.x += this.dir
+        if(this.x <= 0){
+            this.x = 0
+        }else if(this.x >= 820){
+            this.x = 820
+        }
+    }
 }
-    
-    class Tiro extends Obj(){
-        
-        colid(objeto){
-            if((this.x < objeto.x + objeto.w)&&
-            (this.x+this.w > objeto.x )&&
-            (this.y < objeto.y + objeto.h)&&
-            (this.y + this.h > objeto.y)){
-                return true}
-                else{
-                    false
-                }
-            }
+
+class Player extends Obj{
+    dir = 0
+    pts = 0
+    vida = 5
+
+    mov(){
+        this.x += this.dir
+        if(this.x <= 0){
+            this.x = 0
+        }else if(this.x >= 820){
+            this.x = 820
+        }
     }
     
+}
+class Tiro extends Obj{
+    des_img(){
+        let img = new Image()
+        img.src = this.a
+        des.drawImage(img, this.x, this.y, this.w, this.h)
+    }
+    
+        mov(){
+            this.y -= 10
+        }
+    }
+
     class Text{
         
     des_text(text,x,y,cor,font){
@@ -40,4 +65,8 @@ class Armas extends Obj(){
     des.font = font
     des.fillText(text, x, y)
         }
+    }
+
+    class botao extends Obj{
+
     }
--- Two_main.js
+++ Two_main.js
@@ -1,13 +1,71 @@
 let des =document.getElementById('des').getContext('2d')
 
-let cajado = new Armas (300,150,90,90,'./assets/cajado1.png')
+let player = new Player(100,500,50,50,'red') 
+let cajado = new Armas (player.x-40, player.y-60, 80, 80,'./assets/cajado1.png')
+let text_pontos = new Text(100,100,50,50,'red') 
+let text_life = new Text(100,100,50,50,'red') 
+let text_armas = new Text(100,100,50,50,'red')
+let linha = new Obj(880, 2, 10,640, 'white')
+let quadrado = new Obj(1050, 400, 100,100, 'white')
+let quadrado2 = new Obj(1060, 410, 80,80, 'black')
+let tiro = new Tiro()
+
+
+document.addEventListener('keydown',(e)=>{
+    // console.log(e.key)
+    if(e.key === 'a'){
+        player.dir -= 5
+    }else if(e.key === 'd'){
+        player.dir += 5
+    }
+})
+document.addEventListener('keyup', (e)=>{
+    if(e.key === 'a'){
+        player.dir = 0
+    }else if(e.key === 'd'){
+        player.dir = 0
+    }
+})
+
+document.addEventListener('keydown',(e)=>{
+    // console.log(e.key)
+    if(e.key === 'a'){
+        cajado.dir -= 5
+    }else if(e.key === 'd'){
+        cajado.dir += 5
+    }
+})
+document.addEventListener('keyup', (e)=>{
+    if(e.key === 'a'){
+        cajado.dir = 0
+    }else if(e.key === 'd'){
+        cajado.dir = 0
+    }
+})
+
 
 function desenha(){
+    player.des_obj()
     cajado.des_img()
+    text_life.des_text('Vida: ',920, 50, 'White', '26px Times')
+    text_pontos.des_text('Pontos: ',920, 180, 'White', '26px Times')
+    text_armas.des_text('Armas', 1060, 380, 'white','26px Times')
+    linha.des_obj()
+    quadrado.des_obj()
+    quadrado2.des_obj()
+    
 }
+
+function atualiza(){
+    player.mov()
+    cajado.mov()
+}
+
 
 function main(){
+    des.clearRect(0,0,950,630)
     desenha()
+    atualiza()
 }
 
-setInterval(main,10)
+main()
